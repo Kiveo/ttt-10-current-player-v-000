c16838a9fb701abd4bef915bccf9ea7be92f83d9
Maverick Tesini <mavericktesini@gmail.com>
2018-02-20 18:44:04 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,11 @@
-def turn_count
-
+def turn_count(board)
+  counter = 0
+  board.each |turn| do
+    if turn == "O" || turn =="X"
+      counter +=1
+    end    
+  end
+  puts "Counter indicates it is turn: #{counter}"
 end
 
 def current_player
